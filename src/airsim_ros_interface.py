a8bc4282fb93d39cd814ff6729fc95e8e337be90
Simen Gangstad <simen.gangstad@me.com>
2020-02-14 21:14:24 +0100

    Add point cloud publishing

--- src/airsim_ros_interface.py
+++ src/airsim_ros_interface.py
@@ -9,20 +9,18 @@ import signal
 import sys
 import msgpackrpc 
 
-from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped, Point32
+from sensor_msgs.msg import Image, LaserScan, PointCloud
 
 current_pose = PoseStamped()
-client = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", 41451), timeout = 3600, pack_encoding = 'utf-8', unpack_encoding = 'utf-8')
+rpc_client = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", 41451), timeout = 3600, pack_encoding = 'utf-8', unpack_encoding = 'utf-8')
 
 
 def poseCallback(msg):
     global current_pose
     current_pose = msg
 
-def generateLaserScan(data):
+def publishLaserScanFromData(data):
     scan = LaserScan()
     scan.header.frame_id = "base_link"
     scan.angle_max =  math.pi 
@@ -54,40 +52,67 @@ def generateLaserScan(data):
         if rng < scan.ranges[index]:
             scan.ranges[index] = rng
 
-    return scan
+    scan_publisher.publish(scan)
+
+def publishPointCloudFromData(data):
+    point_cloud = PointCloud()
+    # TODO: Which frame?
+    point_cloud.header.frame_id = "map"
+
+    for i in range(0, len(data.point_cloud), 3):
+        # The lidar data is in NED so we have to convert to ENU and transform to local frame
+        # TODO: Which reference frame
+        x = data.point_cloud[i + 1] - current_pose.pose.position.x 
+        y = data.point_cloud[i] - current_pose.pose.position.y
+        z = -data.point_cloud[i + 2] - current_pose.pose.position.z
+
+        point = Point32()
+        point.x = x
+        point.y = y
+        point.z = z
+
+        point_cloud.points.append(point)
+
+    point_cloud_publisher.publish(point_cloud)
+
 
 def sendBackPropellerThrustSignal(value):
-    client.call('setBackPropellerControlSignal', value, "PX4")
+    rpc_client.call('setBackPropellerControlSignal', value, "PX4")
+
+def collidesWithModule():
+    return rpc_client.call("collidesWithModule", "SimpleFlight")
 
 def signal_handler(sig, frame):
     # Wait until AirSim closes the connnection
     rospy.loginfo("Quitting, stopping connection to AirSim, please wait...")
-    imu_client.reset()
+    image_client.reset()
     rospy.signal_shutdown("Successfully shut down the airsim node")
 
 def main():
-    global imu_client
+    global image_client, scan_publisher, point_cloud_publisher
+
     rospy.init_node('airsim_ros_interface', disable_signals=True)
     signal.signal(signal.SIGINT, signal_handler)
 
-    imu_publisher = rospy.Publisher("/airsim/imu/data", Imu, queue_size=1)
     image_publisher = rospy.Publisher("/airsim/camera/image", Image, queue_size=1)
     scan_publisher = rospy.Publisher("/scan", LaserScan, queue_size=1)
+    point_cloud_publisher = rospy.Publisher("/airsim/point_cloud", PointCloud, queue_size=1)
 
     rospy.Subscriber("/mavros/local_position/pose", PoseStamped, poseCallback)
 
     # We have to set up multiple clients as the AirSim API seems to prefer this, through tests this causes UE to 
-    # freeze far less
-    imu_client = airsim.MultirotorClient()
+    # freeze far less. No clue why this is the case.
     image_client = airsim.MultirotorClient()
     lidar_client = airsim.MultirotorClient()
+    point_cloud_client = airsim.MultirotorClient()
 
     connected = False
     while not connected:  
         try:
-            imu_client.confirmConnection()
             image_client.confirmConnection()
             lidar_client.confirmConnection()
+            point_cloud_client.confirmConnection()
+
             connected = True
         except:
             rospy.logerr("Could not connect to AirSim, is the simulation running?")
@@ -95,45 +120,26 @@ def main():
 
     rospy.loginfo("Starting publishing!")
 
-    rate = rospy.Rate(rospy.get_param("~framerate", 100.0))
-
-    last_imu_time = rospy.get_time()
-    imu_time = 1.0 / rospy.get_param("~imu_publishing_frequency", 100.0);
+    rate = rospy.Rate(rospy.get_param("~rate"))
 
     last_scan_time = rospy.get_time()
-    scan_time = 1.0 / rospy.get_param("~scan_publishing_frequency", 10.0)
+    scan_time = 1.0 / rospy.get_param("~scan_publishing_frequency")
 
     last_image_time = rospy.get_time()
-    image_time = 1.0 / rospy.get_param("~image_publishing_frequency", 30.0) 
+    image_time = 1.0 / rospy.get_param("~image_publishing_frequency") 
+
+    last_point_cloud_time = rospy.get_time()
+    point_cloud_time = 1.0 / rospy.get_param("~point_cloud_publishing_frequency") 
+
 
     frame_id = rospy.get_param("~frame_id", "base_link")
 
-    camera_name = rospy.get_param("~camera_name", "MyCamera1")
-    image_width = rospy.get_param("~image_width", 256)
-    image_height = rospy.get_param("~image_height", 144)
+    camera_name = rospy.get_param("~camera_name")
+    image_width = rospy.get_param("~image_width")
+    image_height = rospy.get_param("~image_height")
 
     while not rospy.is_shutdown():
-
-        if rospy.get_time() - last_imu_time >= imu_time:
-
-            imu_data = imu_client.getImuData()
-
-            imu_msg = Imu()
-            imu_msg.header.frame_id = frame_id
-            imu_msg.header.stamp = rospy.Time.now()
-            imu_msg.orientation.x = imu_data.orientation.x_val
-            imu_msg.orientation.y = imu_data.orientation.y_val
-            imu_msg.orientation.z = imu_data.orientation.z_val
-            imu_msg.orientation.w = imu_data.orientation.w_val
-            imu_msg.angular_velocity.x = imu_data.angular_velocity.x_val
-            imu_msg.angular_velocity.y = imu_data.angular_velocity.y_val
-            imu_msg.angular_velocity.z = imu_data.angular_velocity.z_val
-            imu_msg.linear_acceleration.x = imu_data.linear_acceleration.x_val
-            imu_msg.linear_acceleration.y = imu_data.linear_acceleration.y_val
-            imu_msg.linear_acceleration.z = imu_data.linear_acceleration.z_val
-
-            imu_publisher.publish(imu_msg)
-
+        
         if rospy.get_time() - last_image_time >= image_time:
             responses = image_client.simGetImages([airsim.ImageRequest(camera_name, airsim.ImageType.Scene, False, False)])
 
@@ -154,16 +160,19 @@ def main():
             last_image_time = rospy.get_time()
 
         if rospy.get_time() - last_scan_time >= scan_time:
-            scan = generateLaserScan(lidar_client.getLidarData())
-            scan_publisher.publish(scan)
+            publishLaserScanFromData(lidar_client.getLidarData(lidar_name = "Lidar2D", vehicle_name = "PX4"))
             last_scan_time = rospy.get_time()
+
+        if rospy.get_time() - last_point_cloud_time >= point_cloud_time:
+            publishPointCloudFromData(point_cloud_client.getLidarData(lidar_name = "Lidar3D", vehicle_name = "PX4"))
+            last_point_cloud_time = rospy.get_time()
 
         rate.sleep()
 
     rospy.loginfo("Quitting, stopping connection to AirSim, please wait...")
     
     # Safely wrap up the connection to AirSim in order to prevent freezes in UE
-    imu_client.reset()
+    image_client.reset()
 
 if __name__ == '__main__':
     try:
